a5a6ff44a5d2c4408c03e6697f7a05526c71a716
MrSumada <ricksamericana.com>
2022-03-16 12:06:33 -0400

    add login

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-const express = require('express');
 const path = require('path');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const express = require('express');
+// const routes = require('./controllers');
+const session = require('express-session')
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const sess = {
+    secret: 'Super Secret Thing',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 
@@ -14,8 +32,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // turn on routes
-app.use(routes);
+app.use(require('./controllers'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
