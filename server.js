80a878ca81cd1c4afafdfbd8630c2d0b75776d49
MrSumada <ricksamericana.com>
2022-03-16 15:51:03 -0400

    clean logged in bug

--- server.js
+++ server.js
@@ -30,11 +30,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controllers/'));
 
-// turn on routes
-app.use(require('./controllers'));
-
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
