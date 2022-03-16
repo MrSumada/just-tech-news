8b191371557b78f4d369328c37bb510d444b66c0
MrSumada <ricksamericana.com>
2022-03-16 13:54:03 -0400

    add comment functionality

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
 const path = require('path');
 const express = require('express');
-// const routes = require('./controllers');
-const session = require('express-session')
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Super Secret Thing',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
 
 const hbs = exphbs.create({});
 
-
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
